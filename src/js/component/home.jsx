29ef1cb74a2637405affa3924981e4a0059b269a
FaithIsWork11 <faithiswork@duck.com>
2024-07-10 14:07:33 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+const [red , setRed ] = useState("red")
+const [yellow , setYellow ] = useState("yellow")
+const [green , setGreen ] = useState("green")
+
+      const [glored , glowRed] = useState('')
+      const [gloyellow , glowYellow ] = useState('')
+	  const [glogreen , glowGreen ] = useState('')
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className =" mt-4 ">
+              <div className="pipe mx-auto"></div>
+			  <div className="bg-black pipe2 mx-auto rounded p-4">
+                    <div className =" circle bg-danger mx-auto "></div>
+					<div className =" circle bg-warning mx-auto "></div>
+					<div className =" circle bg-success mx-auto "></div>
+
+
+			  </div>
 		</div>
 	);
 };
